741d21fd0569863a0d7203230d5a1516b1adbc43
Phuc Nguyen <ntphuc@cmc-outsource.com>
2015-01-21 13:40:35 +0700

    first

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+	$('.top-menu').bxSlider({
+		slideWidth: 220,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    slideMargin: 20,
+	    pager: false,
+	    
+	});
+});
+
+jQuery(window).load(function(){
+
+});
